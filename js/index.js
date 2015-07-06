321e012ca57a4afc7a6d7bc159b14561541a119b
grahamegan <grahamegan@Yexts-MacBook-Pro.local>
2015-07-06 17:31:22 -0400

    almost done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,80 @@
+
+var size;
+var coat;
+var tail;
+
 $(document).ready(function(){
 
 	console.log("is this thing on?");
 
-	$(".size img").click(hideSize);
+	$(".size img").click(identifySize);
+	$(".coat img").click(identifyCoat);
+	$(".tail img").click(identifyTail);
+	$("button").click(idogtify);
 
-	function hideSize(){
+	function identifySize(){
 		console.log("size clicked");
-		var size = $(".size").val()
-		console.log(size)
+		
+		size = $(this).attr("alt");
+		
+		console.log(size);
+		
 		$(".size").hide();
 		$(".coat").show();
+	}
+
+	function identifyCoat(){
+		console.log("coat clicked");
+
+		coat = $(this).attr("alt");
+
+		console.log(coat);
+
+		$(".coat").hide();
+		$(".tail").show();
+	}
+
+	function identifyTail(){
+		console.log("tail clicked");
+
+		tail = $(this).attr("alt");
+
+		console.log(tail);
+
+		$(".tail").hide();
+		$("button").show();
+		$("body").addClass("black");
+
+	}
+
+	function idogtify(){
+		console.log("idogtify clicked");
+		// $("button").hide();
+
+		if (size == "small") {
+			if (coat == "long"){
+				if (tail == "bob"){
+					$("button").html("It's a Corgi!");
+					console.log("it's a corgi!");}
+				else { if (tail == "docked"){
+					$("button").html("It's a Field Spaniel!");
+					console.log("it's a field spaniel!");}
+				else { if (tail == "screw"){
+					$("button").html("It's a Chow Chow!");
+					console.log("it's a chow chow!");}
+				}
+				}
+			
+			} else { if (coat == "smooth"){
+				if (tail == "flagpole"){
+					$("button").html("It's a Beagel!");
+					console.log("it's a beagel!");}
+				else { if (tail == "screw"){
+					$("button").html("It's a Pug!");
+					console.log("it's a pug!");}
+				}
+				}
+			}
+		}
 	}
 })
